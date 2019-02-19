029ba32096c7f0040606444afd3a538fe6488022
Luan Carlos Martins dos Santos <47756761+LuanSanMartins@users.noreply.github.com>
2019-02-19 16:47:57 -0300

    Print string by two way different
    This algorithm print a "string" by two way different. This, print the "string" whole, and latter by latter.

---
+++ PrintStringByTwoWay.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    char word[] = "civic";
+    int i;
+
+    for(i = 0; i < 5; i++){
+
+        printf("%c\n", word[i]);
+
+    }
+
+    printf("%s\n", word);
+
+    system("pause");
+    return(0);
+
+}
