03e3a16e91bffed78dad161249a2936b575e6d52
Luan Carlos Martins dos Santos <47756761+LuanSanMartins@users.noreply.github.com>
2019-02-18 23:39:28 -0300

    Print of sequence of ten numbers
    This algorithm print a sequence of ten numbers.

---
+++ SequenceOfTenNumbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int i;
+
+    for(i=0; i<10; i++){
+        printf("The number %d of sequence is: %d\n", i, i);
+    }
+
+    system("pause");
+    return(0);
+
+}
